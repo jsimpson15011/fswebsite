cf952ea534e2ff439518aeba8f3321a133627a33
Joe Simpson <joseph.franklin.simpson@gmail.com>
2017-07-21 21:56:46 -0600

    Php tests

--- test.php
+++ test.php
@@ -1,15 +1,40 @@
+<div class="weddingwordbox">
 <?php  
 include_once "library.php";
-for ($i=0; $i < 10; $i++) {
-	minus_one(5); 
-	$input= "lndKDJkjk";
-	$name= strtolower($input);
-	if ($i==1) {
-		continue;
-	}
-	echo ' '.$name. ' '. $input;
-	echo $i;
-}
+
 minus_one(2);
 
+$object1= new Membership ();
+$object2= clone $object1;
+
+$object1-> date='7/12';
+$object1-> name='Johnt';
+$object1-> time='2';
+$object2-> name='ronk';
+print_r($object1);
+echo "</br>" .$object1-> name .'</br> ';
+echo $object2-> name;
+
+echo $object1->save_teetimes();
+echo $object1->save_teetimes_original();
+
+class Teetime
+{
+	public $date, $time, $name;
+	function save_teetimes(){
+		return $this->date;
+	}
+}
+
+class Membership extends Teetime
+{
+	function save_teetimes(){
+		echo "no";
+	}
+	function save_teetimes_original(){
+		parent::save_teetimes();
+	}
+}
 ?>
+</div>
+<link rel="stylesheet" type="text/css" href="CSS/styles.css">
